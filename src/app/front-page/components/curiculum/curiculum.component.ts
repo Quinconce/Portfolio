0d082fd6c51e9c16ec3e7a4bcdbe597045f72014
mohamed-aine <mabelkahla@methodinthemadness.eu>
2022-11-14 16:50:50 +0100

    composent + normalize

---
+++ src/app/front-page/components/curiculum/curiculum.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'curiculum',
+  templateUrl: './curiculum.component.html',
+  styleUrls: ['./curiculum.component.scss']
+})
+export class CuriculumComponent {
+}
---
+++ src/app/front-page/components/other-project/other-project.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'other-project',
+  templateUrl: './other-project.component.html',
+  styleUrls: ['./other-project.component.scss']
+})
+export class OtherProjectComponent {
+}
