7e004fc61d564e743c6afffc666e7fc51003668a
Mina <minasamwail@gmail.com>
2022-11-15 15:00:59 +0100

    installation des packages three et ventura + test

--- src/app/front-page/components/home/home.component.ts
+++ src/app/front-page/components/home/home.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component , AfterViewInit} from '@angular/core';
+declare var VANTA: any;
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit  {
+
+  ngAfterViewInit(): void {
+    VANTA.BIRDS({
+      el: '#vanta', // element selector string or DOM object reference
+      color: 0x000000,
+      waveHeight: 20,
+      shininess: 50,
+      waveSpeed: 1.5,
+      zoom: 0.75
+    })
+  }
 }
